3a5d45c5289cdb0d7e175e468943b0d7febb4881
nductri44 <nductri44@gmail.com>
2023-09-29 15:51:36 +0700

    Rails Chapter 08: Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home', to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   get '/signup', to: 'users#new'
   get '/users/[:id]', to: 'users#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
